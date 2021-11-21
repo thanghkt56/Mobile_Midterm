2ef2018f8585841b99fafc019568609e5c2db098
thanghkt56 <thangdapchai@gmail.com>
2021-11-21 11:35:41 +0700

    UI minor fix & Add share button

--- app/src/main/java/com/example/loginauthfirebase/AddImageActivity.java
+++ app/src/main/java/com/example/loginauthfirebase/AddImageActivity.java
@@ -5,6 +5,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -13,7 +14,6 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
@@ -21,23 +21,24 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AddImageActivity extends AppCompatActivity {
-    Button uploadBtn;
+    Button uploadBtn,shareBtn;
     EditText inputISO,inputF,inputMM,inputSpeed,inputDate;
     ImageView imageView;
     ProgressDialog progressDialog;
@@ -47,12 +48,20 @@ public class AddImageActivity extends AppCompatActivity {
     FirebaseStorage mImageStore;
     Uri imageUri;
     String userID;
+
+    //Khai bao de share
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_image);
 
-        uploadBtn = findViewById(R.id.uploadBtn);
+
+        uploadBtn = findViewById(R.id.btnUpload);
         inputISO = findViewById(R.id.inputISO);
         inputF = findViewById(R.id.inputF);
         inputMM = findViewById(R.id.inputMM);
@@ -65,6 +74,27 @@ public class AddImageActivity extends AppCompatActivity {
         mUser = mAuth.getCurrentUser();
         mStore = FirebaseFirestore.getInstance();
         mImageStore = FirebaseStorage.getInstance();
+
+
+        //Share link
+        callbackManager = CallbackManager.Factory.create();
+        shareBtn=(Button) findViewById(R.id.btnShareFB);
+        shareDialog =new ShareDialog(this);
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShareLinkContent shareLinkContent =new ShareLinkContent.Builder()
+                        .setContentUrl(Uri.parse("https://firebasestorage.googleapis.com/v0/b/login-auth-firebase-40e1b.appspot.com/o/1637418199115.jpg?alt=media&token=a63170b7-7579-43bb-a39b-cb9d31188ad2"))
+                        .setShareHashtag(new ShareHashtag.Builder()
+                                .setHashtag("#Aesthetic").build())
+                        .build();//Cai link dang set cung, thay bang URL hinh` nao do vao
+                if(ShareDialog.canShow(ShareLinkContent.class))
+                {
+                    shareDialog.show(shareLinkContent);
+                }
+            }
+        });// Het share Link
+
 
         ActivityResultLauncher<Intent> imageResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -98,65 +128,65 @@ public class AddImageActivity extends AppCompatActivity {
     }
 
     private void uploadToFirebase(Uri imageUri) {
-        try {
-            String ISO = inputISO.getText().toString();
-            String f = inputF.getText().toString();
-            String MM = inputMM.getText().toString();
-            String Speed = inputSpeed.getText().toString();
-            String Date = inputDate.getText().toString();
+        String ISO = inputISO.getText().toString();
+        String f = inputF.getText().toString();
+        String MM = inputMM.getText().toString();
+        String Speed = inputSpeed.getText().toString();
+        String Date = inputDate.getText().toString();
 
-            if (imageUri == null) {
-                Toast.makeText(AddImageActivity.this, "Please select Image", Toast.LENGTH_SHORT).show();
-            } else if (ISO.isEmpty() || f.isEmpty() || MM.isEmpty() || Date.isEmpty()) {
-                Toast.makeText(AddImageActivity.this, "Please fill all Fields", Toast.LENGTH_SHORT).show();
-            } else {
-                progressDialog.setMessage("Uploading Image...");
-                progressDialog.setTitle("Upload");
-                progressDialog.setCanceledOnTouchOutside(false);
-                progressDialog.show();
+        if (imageUri == null) {
+            Toast.makeText(AddImageActivity.this, "Please select Image", Toast.LENGTH_SHORT).show();
+        } else if (ISO.isEmpty() || f.isEmpty() || MM.isEmpty() || Date.isEmpty()) {
+            Toast.makeText(AddImageActivity.this, "Please fill all Fields", Toast.LENGTH_SHORT).show();
+        } else {
+            progressDialog.setMessage("Uploading Image...");
+            progressDialog.setTitle("Upload");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
 
-                StorageReference fileRef = mImageStore.getReference().child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            StorageReference fileRef = mImageStore.getReference().child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
 
-                fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                progressDialog.dismiss();
-                                Model model = new Model(uri.toString(), ISO, f, MM, Speed, Date);
-                                String markerID = "TestMarkerID";
-                                DocumentReference markerRef = mStore.collection("markers").document(markerID);
-                                CollectionReference IMGRef = mStore.collection("markers").document(markerID).collection("IMG");
+            fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            progressDialog.dismiss();
+                            Model model = new Model(uri.toString(), ISO, f, MM, Speed, Date);
+                            String markerID = "TestMarkerID";
+                            //DocumentReference markerRef = mStore.collection("markers").document(markerID);
+                            CollectionReference IMGRef = mStore.collection("markers").document(markerID).collection("IMG");
 
-                                /*
-                                Map<String, Object> marker = new HashMap<>();
-                                marker.put("name", "Cho Da Lat");
-                                markerRef.set(marker).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void unused) {
-                                        Toast.makeText(AddImageActivity.this, "Add Marker Successful", Toast.LENGTH_SHORT).show();
-                                    }
-                                });*/
+                            /*
+                            Map<String, Object> marker = new HashMap<>();
+                            marker.put("name", "Cho Da Lat");
+                            marker.put("X","11.9428117");
+                            marker.put("Y","108.4366088");
+                            markerRef.set(marker).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(AddImageActivity.this, "Add Marker Successful", Toast.LENGTH_SHORT).show();
+                                }
+                            });*/
 
 
-                                IMGRef.add(model);
-                            }
-                        });
+                            IMGRef.add(model);
+                            Toast.makeText(AddImageActivity.this, "Image uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        progressDialog.dismiss();
-                        Toast.makeText(AddImageActivity.this, "Upload failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        }catch(Exception ex){
-            Log.e("Error: ", ex.getMessage());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(AddImageActivity.this, "Upload failed", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
+
 
     private String getFileExtension(Uri imageUri) {
         ContentResolver cr = getContentResolver();
